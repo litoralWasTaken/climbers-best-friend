16044708c928e242cd0851f503d7d8adc147b5c1
Rodrigo Garcia <litoralwastaken@gmail.com>
2024-06-26 17:56:17 +0200

    Arreglado "pegajosidad" de la navbar

--- resources/js/Components/common/Navbar.vue
+++ resources/js/Components/common/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="fixed .top-0 w-full">
+    <nav class="sticky top-0 w-full">
         <button id="hamburger" @click="toggleNavbar">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="size-6">
