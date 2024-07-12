a32d52fe5623de20072c9abb8e6d0c22e29c4cc6
Rodrigo Garcia <litoralwastaken@gmail.com>
2024-07-12 18:38:54 +0200

    Eliminados imports innecesarios

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet';
 import "leaflet/dist/leaflet.css";
-import Map from '@/Components/map/Map.vue';
 import Navbar from '@/Components/common/Navbar.vue'
 
 
@@ -20,11 +18,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .use(LMap)
-            .use(LTileLayer)
-            .use(LMarker)
             .use(Navbar)
-            .use(Map)
             .mount(el);
     },
     progress: {
